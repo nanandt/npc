c95b17b1c2408488b711fb74cc630858b24c5984
nanandt <rizkyfatih007@gmail.com>
2020-09-13 01:59:15 +0700

    integrasi frontend dan membuat home controller

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,58 @@
+<!-- ##### Footer Area Start ##### -->
+<footer class="footer-area">
+    <div class="container">
+        <div class="row">
+            <!-- Footer Widget Area -->
+            <div class="col-12 col-sm-6 col-lg-3">
+                <div class="footer-widget">
+                    <!-- Logo -->
+                    <a href="index.php" class="foo-logo"><img src="{{ url('frontend/img/core-img/logo2.png') }}" alt=""></a>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+            </div>
+
+
+
+            <!-- Footer Widget Area -->
+            <div class="col-12 col-sm-6 col-lg-3">
+                <div class="footer-widget">
+                    <h6 class="widget-title"></h6>
+                    <!-- Single Blog Post -->
+                    <div class="footer-social-info">
+                        <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+                        <a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>
+                        <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
+                        <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+                        <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Copywrite Area -->
+    <div class="copywrite-area">
+        <div class="container">
+            <div class="row">
+                <!-- Copywrite Text -->
+                <div class="col-12 col-sm-6">
+                    <p class="copywrite-text">
+                      Copyright &copy;<script>document.write(new Date().getFullYear());</script> National Paralympic Committee of Indonesia | All rights reserved
+                    </p>
+                </div>
+                <div class="col-12 col-sm-6">
+                    <nav class="footer-nav">
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Privacy</a></li>
+                            <li><a href="#">Advertisement</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- ##### Footer Area End ##### -->
