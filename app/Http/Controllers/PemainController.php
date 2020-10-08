322dce59a322f44d6b9cc1a4242eb348741624ff
nanandt <rizkyfatih007@gmail.com>
2020-10-08 15:53:23 +0700

    integrasi halaman pemain di cabor

--- app/Http/Controllers/PemainController.php
+++ app/Http/Controllers/PemainController.php
@@ -10,10 +10,12 @@ class PemainController extends Controller
 {
   public function index(Request $request)
   {
-    $items = Pemain::with('cabang_olahraga')->where('cabang_olahraga_id', $request->cabang_olahraga_id)->get();
+    $cabors = CabangOlahraga::with('pemains')->get();
+    $items = Pemain::with('cabang_olahraga')->where('cabang_olahraga_id', $request->cabang_olahraga_id)->take(4)->get();
     // dd($items);
     return view('pages.pemain', [
-        'items' => $items
+        'items' => $items,
+        'cabors' => $cabors
     ]);
   }
 }
